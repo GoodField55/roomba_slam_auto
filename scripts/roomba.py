cede413fc3e38f3ce4791eb3af1efa3b1efdfa03
GoodField55 <goodfield055@gmail.com>
2020-04-26 06:10:59 +0000

    Add roomba.py for auto mode(trial)

--- scripts/roomba.py
+++ scripts/roomba.py
@@ -2,67 +2,68 @@
 import rospy,copy,math,random
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
+from ca_msgs.msg import Bumper
 
 class Roomba():
   def __init__(self):
     self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
-#    self.sensor_values = LightSensorValues()
-#    rospy.Subscriber('lightsensors', LightSensorValues, self.callback)
+    self.bumper_values = Bumper()
+    rospy.Subscriber('bumper', Bumper, self.callback)
 
-#    self.right_flag = False
-#    self.left_flag = False
+    self.right_flag = False
+    self.left_flag = False
 
-#  def callback(self,messages):
-#    self.sensor_values = messages
+  def callback(self,messages):
+    self.bumper_values = messages
 
-#  def wall_front(self,ls):
-#    return ls.left_forward > 50 or ls.right_forward > 50
+  def wall_front(self,ls):
+    return ls.light_signal_center_left > 100 or ls.light_signal_center_right > 100
 
-#  def too_right(self,ls):
-#    return ls.right_side > 50
+  def too_right(self,ls):
+    return ls.light_signal_front_right > 100
 
-#  def too_left(self,ls):
-#    return ls.left_side > 50
+  def too_left(self,ls):
+    return ls.light_signal_front_left > 100
 
   def run(self):
     rate = rospy.Rate(10)	#(20)
     data = Twist()
 
-    data.linear.x = 0.0		#0.1
-    data.angular.z = 0.0
+    data.linear.x = 0.1
 
-    z = 0.0			#0.5		#math.pi / 3.0
-
+    z = math.pi / 3.0
     data.angular.z = z
 
-
     while not rospy.is_shutdown():
-#      if self.wall_front(self.sensor_values):
-#        if self.right_flag:
-#            data.angular.z = -1 * z
-#        elif self.left_flag:
-#            data.angular.z = z
-#        elif random.random() > 0.5:
-#            data.angular.z = -1 * z
-#            self.left_flag = False
-#            self.right_flag = True      
-#        else:
-#          data.angular.z = z
-#          self.left_flag = True
-#          self.right_flag = False
-#      elif self.too_right(self.sensor_values):
-#        data.angular.z = z
-#        self.left_flag = True
-#        self.right_flag = False
-#      elif self.too_left(self.sensor_values):
-#        data.angular.z = -1 * z
-#        self.left_flag = False
-#        self.right_flag = True
-#      else:
-#        data.angular.z = 0
-#        self.left_flag = False
-#        self.right_flag = False
+      if self.wall_front(self.bumper_values):
+        if self.right_flag:
+            data.angular.z = -1 * z
+        elif self.left_flag:
+            data.angular.z = z
+        elif random.random() > 0.5:
+            data.angular.z = -1 * z
+            self.left_flag = False
+            self.right_flag = True      
+        else:
+          data.angular.z = z
+          self.left_flag = True
+          self.right_flag = False
+      elif self.too_right(self.bumper_values):
+        data.angular.z = z
+        self.left_flag = True
+        self.right_flag = False
+      elif self.too_left(self.bumper_values):
+        data.angular.z = -1 * z
+        self.left_flag = False
+        self.right_flag = True
+      else:
+        data.angular.z = 0
+        self.left_flag = False
+        self.right_flag = False
+
+      data.linear.x = 0.0     # dummy
+      data.angular.z = 0.0    #dummy
 
       rospy.loginfo("vel=%f, ang=%f\n", data.linear.x, data.angular.z)
       self.cmd_vel.publish(data)
@@ -70,9 +71,5 @@ class Roomba():
 
 if __name__ == '__main__':
   rospy.init_node('roomba')
-#  rospy.wait_for_service('/motor_on')
-#  rospy.wait_for_service('/motor_off')
-#  rospy.on_shutdown(rospy.ServiceProxy('motor_off',Trigger).call)
-#  rospy.ServiceProxy('motor_on',Trigger).call()
   Roomba().run()
 
